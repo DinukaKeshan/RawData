936158fd77c5330b3a1f186543b500fcbd294399
Dinuka Keshan <dinukakeshan1012@gmail.com>
2025-01-04 22:36:16 +0530

    message

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/msg', (req, res)=>{
-    const uname = req.body.username;
-    const message = {username : uname, userMsg : "Hello world"}
-    res.json(message);
-})
+mongoose
+  .connect("mongodb+srv://Dinuka:Dinuka2001@cluster0.r1eez.mongodb.net/mern")
+  .then(() => console.log("mongoose connected"))
+  .catch(() => console.log("connection failed"));
 
+const userSchema = mongoose.Schema({
+  name: String,
+  age: Number,
+  email: String,
+  city: String,
+  profession: String,
+});
 
+const User = mongoose.model("users", userSchema);
 
-app.get('/', (req, res)=> {
-    const user = {name: 'John', age: 16, school: 'abc'}
-    res.json(user);
+app.get("/users", async (req, res) => {
+  try {
+    const members = await User.find();
+    res.json(members);
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 app.listen(3000, () => {
